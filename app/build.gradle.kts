b2b8f3aefbabe41f8f8fae11770d997f6a63ca09
Muhamad Masyhuri <sumarnoriski89@gmail.com>
2024-12-06 15:51:32 +0700

    up sdk 35 and creat + read note

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -6,12 +6,12 @@ plugins {
 
 android {
     namespace = "com.example.blackmarketroompresistence"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "com.example.blackmarketroompresistence"
         minSdk = 30
-        targetSdk = 34
+        targetSdk = 35
         versionCode = 1
         versionName = "1.0"
 
@@ -52,4 +52,9 @@ dependencies {
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.6.4")
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.6.4")
     implementation("androidx.room:room-ktx:$room_version")
+
+    implementation("com.google.android.material:material:1.8.0")
+    implementation("androidx.recyclerview:recyclerview:1.3.2")
+    implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.8.7")
+    implementation("androidx.lifecycle:lifecycle-livedata-ktx:2.8.7")
 }
--- app/src/main/java/com/example/blackmarketroompresistence/MainActivity.kt
+++ app/src/main/java/com/example/blackmarketroompresistence/MainActivity.kt
@@ -1,12 +1,26 @@
 package com.example.blackmarketroompresistence
 
+import android.annotation.SuppressLint
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.example.blackmarketroompresistence.room.MainViewModel
+import com.example.blackmarketroompresistence.room.NoteAdapter
+import com.example.blackmarketroompresistence.ui.AddNoteActivity
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var recyclerView: RecyclerView
+    private lateinit var noteAdapter: NoteAdapter
+    private lateinit var mainViewModel: MainViewModel
+    @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -16,5 +30,22 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        val recyclerView = findViewById<RecyclerView>(R.id.recyclerViewNotes)
+        recyclerView.layoutManager = LinearLayoutManager(this)
+
+        val mainViewModel = ViewModelProvider(this).get(MainViewModel::class.java)
+
+        mainViewModel.allNotes.observe(this, Observer { notes ->
+            val noteAdapter = NoteAdapter(notes)
+            recyclerView.adapter = noteAdapter
+        })
+
+        val buttonAddNote: Button = findViewById(R.id.buttonAddNote)
+        buttonAddNote.setOnClickListener {
+            val intent = Intent(this, AddNoteActivity::class.java)
+            startActivity(intent)
+        }
+
     }
 }
